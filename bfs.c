bb0477e4ea53cb0d21ca5ff8bad0ae8c5fed094d
kartik sureja <97840724+kartiksureja@users.noreply.github.com>
2022-07-10 21:38:18 +0530

    Update bfs.c

--- bfs.c
+++ bfs.c
@@ -1,98 +1,38 @@
-//Implement BFS
+//DFS
 #include<stdio.h>
 
-int a[20][20],q[20],visited[20],n,i,j,f=1,r=0;
-
-void bfs(int v)
-
-{
-
-
-
-for(i=1;i<=n;i++){
-
-if(a[v][i] && !visited[i]){
-
-q[++r]=i;
-
-visited[q[r]]=1;
-
-
-
-printf("\n %d->%d",v,i);
-
+int a[20][20],reach[20],n;
+void dfs(int v) {
+	int i;
+	reach[v]=1;
+	for (i=1;i<=n;i++)
+	  if(a[v][i] && !reach[i]) {
+		printf("\n %d->%d",v,i);
+		dfs(i);
+	}
 }
+void main() {
+	int i,j,count=0;
 
-}
-if(f<=r)
+	printf("\n Enter number of vertices:");
+	scanf("%d",&n);
+	for (i=1;i<=n;i++) {
+		reach[i]=0;
+		for (j=1;j<=n;j++)
+		   a[i][j]=0;
+	}
+	printf("\n Enter the adjacency matrix:\n");
+	for (i=1;i<=n;i++)
+	  for (j=1;j<=n;j++)
+	   scanf("%d",&a[i][j]);
+	dfs(1);
+	printf("\n");
+	for (i=1;i<=n;i++) {
+		if(reach[i])
+		   count++;
+	}
+	if(count==n)
+	  printf("\n Graph is connected"); else
+	  printf("\n Graph is not connected");
 
-{
-
-bfs(q[f++]);
-
-}
-
-}
-
-void main(){
-
-int v,c=1;
-
-printf("Enter the no of vertices\n");
-
-scanf("%d",&n);
-
-for(i=1;i<=n;i++)
-
-{
-
-q[i]=0;
-
-visited[i]=0;
-
-}
-
-printf("\n Enter graph data in matrix form:\n");
-
-for(i=1;i<=n;i++){
-
-for(j=1;j<=n;j++)
-
-scanf("%d",&a[i][j]);
-
-printf("\n Enter the starting vertex:");
-
-scanf("%d",&v);
-
-visited[v]=1;
-
-bfs(v);
-
-
-
-for(i=1;i<n;i++){
-
-if(visited[i]){
-
-c++;
-
-//printf("%d ",q[i]);
-
-}
-
-}
-
-if(c!=n){
-
-printf("Graph is not connected\n");
-
-}
-
-else{
-
-printf("Graph is connected\n");
-
-}
-
-}
 }
